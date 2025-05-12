641ad6027b9a5450acae7581b5358b7e4ca67062
Abimael Pereira <abimaelgtr270400@outlook.com>
2025-05-12 20:38:29 -0300

    ajuste

--- src/components/SelectClef/SelectClef.tsx
+++ src/components/SelectClef/SelectClef.tsx
@@ -18,7 +18,7 @@ export default function SelectClef({ onClickClef }: gameNameProps) {
                 <button className="bg-primary text-white w-56 text-center rounded-full px-4 py-2 m-2 hover:bg-primary-dark transition duration-300" onClick={() => onClickClef("bass-clef")}>
                     Clave de Fá
                 </button>
-                <button className="bg-primary text-white w-56 text-center flex items-center justify-center rounded-full px-4 py-2 m-2 hover:bg-primary-dark transition duration-300" onClick={() => onClickClef("bass-clef-octave-down")}>
+                <button className="bg-primary text-white w-56 text-center flex items-center justify-center rounded-full px-4 py-2 m-2 hover:bg-primary-dark transition duration-300" onClick={() => onClickClef("bass-clef")}>
                     Clave de Fá
                     <span className="text-xs ms-1">
                         (8º Abaixo)
