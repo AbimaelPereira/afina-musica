9c08488fea22342728d8abef3d737f49e8b731b2
Abimael Pereira <abimaelgtr270400@outlook.com>
2025-05-12 16:11:48 -0300

    Ajuste erros vercel

--- src/app/games/nota-certa/[clef]/page.tsx
+++ src/app/games/nota-certa/[clef]/page.tsx
@@ -6,24 +6,26 @@ import PlayGameClient from "./PlayGameClient"
 const validClefs = ["treble-clef", "bass-clef", "bass-clef-octave-down"] as const
 type Clef = typeof validClefs[number]
 
-async function loadDataGame(fileName: string) {
-    const data = await readJSON(fileName)
-    return shuffleArray(data)
+interface PageProps {
+  params: {
+    clef: string
+  }
 }
 
-export default async function PlayGame({
-    params,
-  }: {
-    params: { clef: string }
-  }) {
-    const { clef } = params  
+async function loadDataGame(fileName: string) {
+  const data = await readJSON(fileName)
+  return shuffleArray(data)
+}
 
-    if (!validClefs.includes(clef as Clef)) {
-        redirect('/')
-    }
+export default async function PlayGame({ params }: PageProps) {
+  const { clef } = params
 
-    const data = await loadDataGame(`nota-certa/${clef}.json`)
-    const svgPath = "/nota-certa/" + clef
+  if (!validClefs.includes(clef as Clef)) {
+    redirect('/')
+  }
 
-    return <PlayGameClient data={data} svgPath={svgPath} />
+  const data = await loadDataGame(`nota-certa/${clef}.json`)
+  const svgPath = "/nota-certa/" + clef
+
+  return <PlayGameClient data={data} svgPath={svgPath} />
 }
